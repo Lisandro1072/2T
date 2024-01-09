42b2700aa8a85783ba34b083696be5cb3016432e
Lisandro1072 <lisandrotintaya@gmail.com>
2024-01-09 08:15:52 -0400

    creacion

---
+++ src/components/baner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Baner() {
+  return (
+    <section className="Banner">
+      <img src="/Banner _DDD.png" alt="" />
+    </section>
+  );
+}
+
+export default Baner;
---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "./avatar";
+import Baner from "./baner";
+
+function Card() {
+  return (
+    <div className="card">
+      <Baner></Baner>
+      <Avatar></Avatar>
+    </div>
+  );
+}
+
+export default Card;
